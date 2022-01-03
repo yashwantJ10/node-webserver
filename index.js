ad411d381a2b18c1c2958876aa51fe8907d5bd39
Yashwant <yashwant.jadhav@zemosolabs.com>
2022-01-03 17:04:29 +0530

    updated port

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 9000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 9000;
 
-app.get('/webhook/proposal', (req, res) => {
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World !');
+});
+
+app.post('/webhook/proposal', function (req, res) {
+  console.log("req body", req.body);
   res.send('ACK');
 })
 
